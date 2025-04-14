1025afde98bf09611911257d614ff6941594b94a
CrytoGod <145210773+CrytoGod@users.noreply.github.com>
2025-04-14 01:56:26 -0400

    home styles
    majority of the styles is done is to make it responsive and not broken

---
+++ js/modules/trending.js
@@ -0,0 +1,15 @@
+
+
+// const animeList = document.querySelector(".anime-list");
+// const upButton = document.querySelector(".arrow-btn.up");
+// const downButton = document.querySelector(".arrow-btn.down");
+
+// const scrollAmount = 220;
+
+// upButton.addEventListener("click", () => {
+//   animeList.scrollTop -= scrollAmount;
+// });
+
+// downButton.addEventListener("click", () => {
+//   animeList.scrollTop += scrollAmount;
+// });
