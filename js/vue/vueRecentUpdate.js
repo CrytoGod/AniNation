c72df903a448b44e8ad7341f50b9e0eda64574c1
CrytoGod <145210773+CrytoGod@users.noreply.github.com>
2025-04-14 15:08:24 -0400

    homepage
    fully responsive

--- js/vue/vueRecentUpdate.js
+++ js/vue/vueRecentUpdate.js
@@ -5,8 +5,17 @@ const app = Vue.createApp({
             loadingMovies: true
         }
     },
+    // created() {
+    //     fetch('http://localhost/github/AniNation/aniNation-backend/public/movies')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             this.movies = data.slice(0, 12); // Only get first 12 movies
+    //             this.loadingMovies = false;
+    //         })
+    //         .catch(error => console.error(error));
+    // },
     created() {
-        fetch('http://localhost/github/AniNation/aniNation-backend/public/movies')
+        fetch('http://localhost:8888/AniNation/aniNation-backend/public/index.php/movies')
             .then(response => response.json())
             .then(data => {
                 this.movies = data.slice(0, 12); // Only get first 12 movies
